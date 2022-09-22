603a70266ac2486a71e6e6f52a7b19e72c03f864
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-22 14:41:58 +0700

    edited js file

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
 function rectangle(x, y) {
     this.chRong = x;
     this.chDai = y;
-    this.area = function(){
+    this.area = function () {
         return this.chRong * this.chDai
     }
-    this.parameter = function(){
+    this.parameter = function () {
         return (this.chRong + this.chDai) * 2
     }
 }
 
-function createRectangle(){
+function createRectangle() {
     let cRong = +(prompt("Enter width of rectangle"));
     let cDai = +(prompt("Enter height of rectangle"));
     let ctx = document.getElementById("myCanvas").getContext("2d");
     let rect = new rectangle(cRong, cDai);
     ctx.beginPath();
-    ctx.rect(20,20,rect.chRong,rect.chDai);
+    ctx.rect(20, 20, rect.chRong, rect.chDai);
     ctx.stroke();
     let dtich = rect.area();
     let chuvi = rect.parameter();
