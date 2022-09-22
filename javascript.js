d5d039552a8555d356aa25b3890729a466bc1b8a
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-22 14:36:05 +0700

    up html js files

---
+++ javascript.js
@@ -0,0 +1,25 @@
+function rectangle(x, y) {
+    this.chRong = x;
+    this.chDai = y;
+    this.area = function(){
+        return this.chRong * this.chDai
+    }
+    this.parameter = function(){
+        return (this.chRong + this.chDai) * 2
+    }
+}
+
+function createRectangle(){
+    let cRong = +(prompt("Enter width of rectangle"));
+    let cDai = +(prompt("Enter height of rectangle"));
+    let ctx = document.getElementById("myCanvas").getContext("2d");
+    let rect = new rectangle(cRong, cDai);
+    ctx.beginPath();
+    ctx.rect(20,20,rect.chRong,rect.chDai);
+    ctx.stroke();
+    let dtich = rect.area();
+    let chuvi = rect.parameter();
+    document.getElementById("result").innerHTML = "Area: " + dtich + "<br>Parameter: " + chuvi;
+}
+
+createRectangle()
